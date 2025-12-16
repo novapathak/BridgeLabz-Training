4dec59347153417c899debfb23357698a84a4c7d
novapathak <novapathak@gmail.com>
2025-12-17 01:06:22 +0530

    [Nova Pathak] : added sixth file

---
+++ gcr-codebase/program-prerequisites/CalculateSimpleInterest.java
@@ -0,0 +1,9 @@
+class CalculateSimpleInterest {
+	public static void main(String args[]){
+		int Principal = 2000;
+		int Rate = 9;
+		int Time = 2;
+		double SimpleInterest = (Principal * Rate * Time) / 100;
+		System.out.println("Simple Interest is " + SimpleInterest);
+	}
+}	

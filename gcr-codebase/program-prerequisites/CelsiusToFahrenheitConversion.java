f8e4270debb23bfb79ff0a5266239b6b7e338eae
novapathak <novapathak@gmail.com>
2025-12-17 01:03:44 +0530

    [Nova Pathak] : added third file

---
+++ gcr-codebase/program-prerequisites/CelsiusToFahrenheitConversion.java
@@ -0,0 +1,7 @@
+class CelsiusToFahrenheitConversion {
+	public static void main(String args[]){
+		int celsius = 30;
+		int fahrenheit = (celsius * 9/5) + 32;
+		System.out.println("Resultant Fahrenheit is " + fahrenheit);
+	}
+}

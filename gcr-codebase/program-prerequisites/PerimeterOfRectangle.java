34571fbdec04745e6dd0e5b8733b2dc95d216b51
novapathak <novapathak@gmail.com>
2025-12-17 01:07:18 +0530

    [Nova Pathak] : added seventh file

---
+++ gcr-codebase/program-prerequisites/PerimeterOfRectangle.java
@@ -0,0 +1,8 @@
+class PerimeterOfRectangle{
+	public static void main(String args[]){
+		int length = 54;
+		int width = 9;
+		int Perimeter = 2 * (length + width);
+		System.out.println("Perimeter of a Rectangle " + Perimeter);
+	}
+}

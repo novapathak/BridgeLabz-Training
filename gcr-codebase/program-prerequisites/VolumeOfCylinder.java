d16d0c0161368d83955f0d586d0aabb156e26090
novapathak <novapathak@gmail.com>
2025-12-17 01:05:37 +0530

    [Nova Pathak] : added fifth file

---
+++ gcr-codebase/program-prerequisites/VolumeOfCylinder.java
@@ -0,0 +1,8 @@
+class VolumeOfCylinder {
+	public static void main(String args[]){
+		int radius = 6;
+		int height = 7;
+	    double Volume = (3.17) * (radius*radius) * height;
+		System.out.println("volume of cylinder is " + Volume);
+	}
+}

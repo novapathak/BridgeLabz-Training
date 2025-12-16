96186ed98e0fefa879b91c04f279c3667da15cc9
novapathak <novapathak@gmail.com>
2025-12-17 00:56:07 +0530

    [Nova Pathak] : added second file

---
+++ gcr-codebase/program-prerequisites/AddTwoNumbers.java
@@ -0,0 +1,8 @@
+class AddTwoNumbers{
+	public static void main(String []args){
+		int a = 25;
+        int b = 56;
+        int sum	= a + b;
+		System.out.println ("The sum of " + a + " and " + b + " is " + sum);
+	}
+}

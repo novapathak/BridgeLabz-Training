b2f5bb6e1657adbe6d3dec952d857e6460c2ddb4
novapathak <novapathak@gmail.com>
2025-12-18 00:40:40 +0530

    [Nova Pathak]: added eleventh file

---
+++ gcr-codebase/programming-elements/level-one/SamAverageMarks.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class SamAverageMarks {
+	public static void main(String args[]){
+		
+		//Creating variable name to assign number
+		int maths = 94, physics = 95, chemistry = 96, totalMarks= 100;
+		double averageMarks = (maths+physics+chemistry)/100;
+		
+		//Displaying the output
+		System.out.println(" Sam’s average mark in PCM is " + averageMarks);
+	}
+}

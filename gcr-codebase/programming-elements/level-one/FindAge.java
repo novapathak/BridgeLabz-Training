5b8c369acba403547b99a6a4db6c32d3bc3c4577
novapathak <novapathak@gmail.com>
2025-12-18 00:35:27 +0530

    [Nova Pathak] : added fifth file in level one

---
+++ gcr-codebase/programming-elements/level-one/FindAge.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class FindAge {
+	public static void main (String args[]){
+		
+		//creating variable
+		int presentYear = 2024;
+		int yearOfBirth = 2000;
+		
+		// Displaying the age.
+		System.out.println("Harry's age in "+ presentYear + " is "  + (presentYear-yearOfBirth) );
+	}
+}	
+		

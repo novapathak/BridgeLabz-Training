b2a4f2bad8608754a5db0d60d4fc455b6222bde3
novapathak <novapathak@gmail.com>
2025-12-18 00:33:13 +0530

    [Nova Pathak] : added third file in level one

---
+++ gcr-codebase/programming-elements/level-one/DividePen.java
@@ -0,0 +1,9 @@
+public class DividePen {
+	public static void main (String args[]){
+		//creating variable to initialise value
+		int totalPens = 14;
+		int penPerStudent = (int)14/3;
+		int remainingPen = 14%3;
+		System.out.println("The Pen Per Student is " + penPerStudent + " and the remaining pen not distributed is " + remainingPen);
+	}
+}

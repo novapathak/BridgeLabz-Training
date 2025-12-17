92934a32debc4b1b55129e37c650ec658224ed87
novapathak <novapathak@gmail.com>
2025-12-18 00:29:38 +0530

    [Nova Pathak] : added first file in level one

---
+++ gcr-codebase/programming-elements/level-one/ConversionOfKmsToMiles.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ConversionOfKmsToMiles {
+	public static void main (String args[]){
+		//Creating variable for values
+		double kiloMeters = 10.8;
+		double miles = 1.6*kiloMeters;
+		//Displaying the output
+		System.out.println("The distance " + kiloMeters + " km in miles is " + miles);
+	}
+}

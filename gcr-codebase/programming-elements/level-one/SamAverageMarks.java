9f362c72f7bef2e339edabb583723cb2c9026d79
novapathak <novapathak@gmail.com>
2025-12-22 11:40:20 +0530

    [Nova]: added SumOfNaturalNumbersRecursive file in level two

--- gcr-codebase/programming-elements/level-one/SamAverageMarks.java
+++ gcr-codebase/programming-elements/level-one/SamAverageMarks.java
@@ -1,12 +1,28 @@
-import java.util.*;
-public class SamAverageMarks {
-	public static void main(String args[]){
-		
-		//Creating variable name to assign number
-		int maths = 94, physics = 95, chemistry = 96, totalMarks= 100;
-		double averageMarks = (maths+physics+chemistry)/100;
-		
-		//Displaying the output
-		System.out.println(" Sam’s average mark in PCM is " + averageMarks);
-	}
+import java.util.Scanner;
+ 
+class SamAverageMarks {
+ 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+ 
+        // get student score from the user
+        System.out.println("Enter grade");
+        int studentGrade = input.nextInt();
+ 
+        // condition for invalid score
+        if (studentGrade < 0 || studentGrade > 100) {
+            System.err.println("Invalid grade.");
+            System.exit(0);
+        }
+
+        // if studentGrade is 50 or above, display pass and congratulatory 
+        // message else display failed message
+        if (studentGrade >= 50) {            
+            System.out.println("Congratulations!");
+            System.out.println("You passed the examination.");
+        } else {
+            System.out.println("Sorry.\nYou failed the examination.");
+        }
+        input.close();
+    }
 }

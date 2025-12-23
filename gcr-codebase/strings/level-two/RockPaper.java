ddb569a4dbb3a45f9414864b3f9f9d51fa888c83
novapathak <novapathak@gmail.com>
2025-12-23 17:11:07 +0530

    [Nova]:added third file in level two

---
+++ gcr-codebase/strings/level-two/RockPaper.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class RockPaperScissors {
+
+    static String computerChoice() {
+        int r = (int)(Math.random() * 3);
+        return r == 0 ? "rock" : r == 1 ? "paper" : "scissors";
+    }
+
+    static String winner(String user, String comp) {
+        if (user.equals(comp)) return "Draw";
+        if ((user.equals("rock") && comp.equals("scissors")) ||
+            (user.equals("paper") && comp.equals("rock")) ||
+            (user.equals("scissors") && comp.equals("paper")))
+            return "User";
+        return "Computer";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int userWin = 0, compWin = 0;
+
+        System.out.print("Enter number of games: ");
+        int games = sc.nextInt();
+
+        for (int i = 1; i <= games; i++) {
+            System.out.print("rock/paper/scissors: ");
+            String user = sc.next();
+            String comp = computerChoice();
+            String result = winner(user, comp);
+
+            if (result.equals("User")) userWin++;
+            if (result.equals("Computer")) compWin++;
+        }
+
+        System.out.println("User Win %: " + (userWin * 100 / games));
+        System.out.println("Computer Win %: " + (compWin * 100 / games));
+    }
+}

72c432402f5b34b6559376aff9b9c18fe51192b9
novapathak <novapathak@gmail.com>
2025-12-26 17:02:35 +0530

    [Nova] : added Employee.java

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/levelone/Employee.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/levelone/Employee.java
@@ -1,10 +1,31 @@
 package com.bridgelabz.oops.levelone;
 
-public class Employee {
+class Employee {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    // Instance variables
+    String name;
+    int id;
+    double salary;
 
-	}
+    // Constructor to initialize values
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
 
+    // Method to display employee details
+    void displayEmployee() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        // Creating object
+        Employee emp = new Employee("Rohan", 1, 500000);
+
+        // Calling display method
+        emp.displayEmployee();
+    }
 }

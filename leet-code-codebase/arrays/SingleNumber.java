fcd63e4268af65ad57c327db1f94fc1e31965fbb
novapathak <novapathak@gmail.com>
2025-12-29 01:18:35 +0530

    [Nova] : added SingleNumber file in leetcode

---
+++ leet-code-codebase/arrays/SingleNumber.java
@@ -0,0 +1,14 @@
+public class SingleNumber {
+    public static int singleNumber(int[] nums){
+        int result= 0;
+        for(int num:nums){
+            result = result ^ num;
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        int[] nums = {4, 1, 2, 1, 2};
+        int answer = singleNumber(nums);
+        System.out.println("The single number is: " + answer);
+    }
+}

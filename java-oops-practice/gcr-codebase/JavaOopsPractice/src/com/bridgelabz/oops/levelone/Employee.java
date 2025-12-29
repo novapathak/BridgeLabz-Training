903e528780d568d2a8c9f2fcf2f33a6b770bcbc7
novapathak <novapathak@gmail.com>
2025-12-29 16:27:39 +0530

    [Nova] : added Employee file in level one of oops

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/oops/levelone/Employee.java
@@ -0,0 +1,31 @@
+package com.bridgrlabz.oops.levelone;
+
+class Employee {
+
+    // Instance variables
+    String name;
+    int id;
+    double salary;
+
+    // Constructor to initialize values
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    // Method to display employee details
+    void displayEmployee() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        // Creating object
+        Employee emp = new Employee("Rohan", 1, 500000);
+
+        // Calling display method
+        emp.displayEmployee();
+    }
+}

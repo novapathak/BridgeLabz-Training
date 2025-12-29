8c056b133dc2645c500b908ced7b34e0b2cb3f4c
novapathak <novapathak@gmail.com>
2025-12-29 16:28:10 +0530

    [Nova] : added item file in level one of oops

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/oops/levelone/Item.java
@@ -0,0 +1,36 @@
+package com.bridgrlabz.oops.levelone;
+
+public class Item {
+
+    // Item details arrays
+    String[] itemCode;
+    String[] itemName;
+    double[] price;
+
+    // Constructor
+    Item(String[] itemCode, String[] itemName, double[] price) {
+        this.itemCode = itemCode;
+        this.itemName = itemName;
+        this.price = price;
+    }
+
+    // Method to display item details
+    void displayItem(int index) {
+        System.out.println("Item Code: " + itemCode[index]);
+        System.out.println("Item Name: " + itemName[index]);
+        System.out.println("Price: " + price[index]);
+        System.out.println("----------------------");
+    }
+
+    public static void main(String[] args) {
+        String[] codes = {"01AA", "01BB", "02AA"};
+        String[] names = {"Water Bottle", "Rice", "Blackboard"};
+        double[] prices = {500, 700, 400};
+
+        Item item = new Item(codes, names, prices);
+
+        for (int i = 0; i < prices.length; i++) {
+            item.displayItem(i);
+        }
+    }
+}

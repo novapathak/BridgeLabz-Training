8bb39a242cc2eb4c6c9a067f1d35f446b630f5d0
novapathak <novapathak@gmail.com>
2025-12-30 13:02:28 +0530

    [Nova] : added vehicle file in keyword

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/keyword/Vehicle.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.keyword;
+
+public class Vehicle {
+
+    private static double registrationFee = 100.0;
+
+    private final String registrationNumber;
+    private String ownerName;
+    private String vehicleType;
+
+    public Vehicle(String ownerName, String vehicleType, String registrationNumber) {
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+        this.registrationNumber = registrationNumber;
+    }
+
+    public static void updateRegistrationFee(double fee) {
+        registrationFee = fee;
+    }
+
+    public void displayRegistrationDetails() {
+        System.out.println("Owner Name: " + ownerName);
+        System.out.println("Vehicle Type: " + vehicleType);
+        System.out.println("Registration Number: " + registrationNumber);
+        System.out.println("Registration Fee: $" + registrationFee);
+    }
+}

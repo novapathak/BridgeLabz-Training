2133bf52aba0286ceb582974f6967d5133ec99a5
novapathak <novapathak@gmail.com>
2025-12-31 15:17:39 +0530

    [Nova] : added LuckyDraw in scenerio based

---
+++ scenario-codebase/day-five/LuckyDraw.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class LuckyDraw {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of visitors: ");
+        int visitors = sc.nextInt();
+
+        for (int i = 1; i <= visitors; i++) {
+            System.out.print("Visitor " + i + ", enter your number: ");
+            int number = sc.nextInt();
+
+            // Invalid input check
+            if (number <= 0) {
+                System.out.println("Invalid number! Skipping visitor.");
+                continue; // Skip to next visitor
+            }
+
+            // Check divisibility by 3 and 5
+            if (number % 3 == 0 && number % 5 == 0) {
+                System.out.println("Congratulations! You won a gift!");
+            } else {
+                System.out.println("Sorry, better luck next time.");
+            }
+        }
+
+        sc.close();
+    }
+}

25cb2b4c8d047b7d74989710265da6164f8b2616
novapathak <novapathak@gmail.com>
2026-01-01 16:07:06 +0530

    [nova] : added book file in object modelling

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/objectmodelling/levelone/Book.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.objectmodelling.levelone;
+
+public class Book {
+
+    private String title;
+    private String author;
+
+    // Constructor
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    // Method to display book details
+    public void displayBook() {
+        System.out.println("Title: " + title + ", Author: " + author);
+    }
+}

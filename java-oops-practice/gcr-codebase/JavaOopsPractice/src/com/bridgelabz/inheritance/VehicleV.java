d060ceb8575d77ed20ad611c1448dec7c55f2d4a
novapathak <novapathak@gmail.com>
2026-01-01 16:43:46 +0530

    [Nova] : added file in inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/inheritance/VehicleV.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.inheritance;
+
+public class VehicleV  {
+
+    protected int maxSpeed;
+    protected String model;
+
+    public VehicleV(int maxSpeed, String model) {
+        this.maxSpeed = maxSpeed;
+        this.model = model;
+    }
+}

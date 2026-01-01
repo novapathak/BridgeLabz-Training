45b6187a86860da3bb0dd09d403fdc8127c0994c
novapathak <novapathak@gmail.com>
2026-01-01 16:09:17 +0530

    [Nova] : added Course main file in object modelling

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/objectmodelling/levelone/CourseUni.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.objectmodelling.levelone;
+
+
+public class CourseUni {
+    String courseName;
+
+    public CourseUni(String courseName) {
+        this.courseName = courseName;
+    }
+}

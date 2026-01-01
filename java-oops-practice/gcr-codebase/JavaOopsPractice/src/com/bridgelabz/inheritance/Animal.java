6aed1acafd5710e251c804c5f913d3445e626ff6
novapathak <novapathak@gmail.com>
2026-01-01 16:33:18 +0530

    [Nova] : added file in inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/inheritance/Animal.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.inheritance;
+
+public class Animal {
+
+    protected String name;
+    protected int age;
+
+    // Constructor
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method to be overridden
+    public void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}

8aed925673bd0963052bddef2d7e4583f58e3d04
novapathak <novapathak@gmail.com>
2026-01-01 16:34:01 +0530

    [Nova] : added file in inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/inheritance/BankAccount.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.inheritance;
+
+public class BankAccount {
+
+    protected String accountNumber;
+    protected double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void displayAccountType() {
+        System.out.println("Generic Bank Account");
+    }
+}

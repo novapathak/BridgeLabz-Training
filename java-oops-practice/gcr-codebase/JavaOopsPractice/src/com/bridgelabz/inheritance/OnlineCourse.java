97110156b17470fc29f0e73003845202f7efa788
novapathak <novapathak@gmail.com>
2026-01-01 16:39:26 +0530

    [Nova] : added file in inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/inheritance/OnlineCourse.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.inheritance;
+
+public class OnlineCourse extends Course {
+
+    protected String platform;
+    protected boolean isRecorded;
+
+    // Constructor
+    public OnlineCourse(String courseName, int duration,
+                        String platform, boolean isRecorded) {
+        super(courseName, duration);
+        this.platform = platform;
+        this.isRecorded = isRecorded;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Platform    : " + platform);
+        System.out.println("Recorded    : " + isRecorded);
+    }
+}

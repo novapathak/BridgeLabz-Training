a31ff116d117a4462bbe46f9914f398719b7451a
novapathak <novapathak@gmail.com>
2026-01-01 16:26:46 +0530

    [Nova] : added file in encapsulation

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/encapsulation/FullTimeEmployee.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.encapsulation;
+
+public class FullTimeEmployee extends Employee implements Department {
+
+    private String department;
+
+    public FullTimeEmployee(int id, String name, double salary) {
+        super(id, name, salary);
+    }
+
+    public double calculateSalary() {
+        return baseSalary;
+    }
+
+    public void assignDepartment(String dept) {
+        this.department = dept;
+    }
+
+    public String getDepartmentDetails() {
+        return department;
+    }
+}

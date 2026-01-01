960290a5aae21b3d35369cd96de830f0307bc356
novapathak <novapathak@gmail.com>
2026-01-01 16:44:07 +0530

    [Nova] : added file in inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/inheritance/Waiter.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.inheritance;
+
+public class Waiter extends PersonP implements Worker {
+
+    public Waiter(String name, int id) {
+        super(name, id);
+    }
+
+    @Override
+    public void performDuties() {
+        System.out.println("Waiter " + name + " is serving customers.");
+    }
+}

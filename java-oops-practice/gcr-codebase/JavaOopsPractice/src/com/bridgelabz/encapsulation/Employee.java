41b50ea24c734fcb442a9876b9d79c7833a559a1
novapathak <novapathak@gmail.com>
2026-01-01 16:25:52 +0530

    [Nova] : added file in encapsulation

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/encapsulation/Employee.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.encapsulation;
+
+public abstract class Employee {
+
+    private int employeeId;
+    private String name;
+    protected double baseSalary;
+
+    public Employee(int employeeId, String name, double baseSalary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    public abstract double calculateSalary();
+
+    public void displayDetails() {
+        System.out.println("ID: " + employeeId + ", Name: " + name +
+                ", Salary: " + calculateSalary());
+    }
+}

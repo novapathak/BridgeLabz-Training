5a9dc7885927f7e138bad13302b29a0b0264de8d
novapathak <novapathak@gmail.com>
2026-01-01 16:31:18 +0530

    [Nova] : added file in encapsulation

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/encapsulation/Truck.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.encapsulation;
+
+public class Truck extends Vehicle implements Insurable {
+
+    private String insurancePolicyNumber;
+
+    public Truck(String number, double rate, String policyNo) {
+        super(number, "Truck", rate);
+        this.insurancePolicyNumber = policyNo;
+    }
+
+    @Override
+    public double calculateRentalCost(int days) {
+        // Extra charge for heavy vehicles
+        return (rentalRate * days) + 1000;
+    }
+
+    @Override
+    public double calculateInsurance() {
+        return 1000;
+    }
+
+    @Override
+    public String getInsuranceDetails() {
+        return "Truck Insurance Policy: " + insurancePolicyNumber;
+    }
+}

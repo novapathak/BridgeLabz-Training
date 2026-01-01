8465202e4758ac5b9f2f2afa603652c5fe0d363c
novapathak <novapathak@gmail.com>
2026-01-01 16:37:34 +0530

    [Nova] : added file in inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/inheritance/Dog.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.inheritance;
+
+public class Dog extends Animal {
+
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    public void makeSound() {
+        System.out.println("Dog barks ");
+    }
+}

e39f5444c69783d7e327d2f15b8203f2c7ea81e6
novapathak <novapathak@gmail.com>
2026-01-01 16:43:24 +0530

    [Nova] : added file in inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/inheritance/VehicleMain.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.inheritance;
+
+public class VehicleMain {
+
+    public static void main(String[] args) {
+
+        Vehicle[] vehicles = new Vehicle[3];
+
+        vehicles[0] = new Car(180, "Petrol", 5);
+        vehicles[1] = new Truck(120, "Diesel", 12.5);
+        vehicles[2] = new Motorcycle(160, "Petrol", true);
+
+        for (Vehicle v : vehicles) {
+            v.displayInfo(); // Runtime Polymorphism
+        }
+    }
+}

8fd1ede89a6ead993c8fabb2fcc37e546c7d4712
novapathak <novapathak@gmail.com>
2026-01-01 16:40:53 +0530

    [Nova] : added file in inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsPractice/src/com/bridgelabz/inheritance/PersonP.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.inheritance;
+
+public class PersonP {
+
+    protected String name;
+    protected int id;
+
+    public PersonP(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}

af822005a1bfc3f8e279dc3cf1ca7c443b2d6b40
novapathak <novapathak@gmail.com>
2026-01-05 17:30:31 +0530

    [Nova] : added Vehicle file

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day01/vehiclerental/Vehicle.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day01.vehiclerental;
+
+class Vehicle {
+    protected String number;
+    protected double baseRate;
+
+    Vehicle(String number, double baseRate) {
+        this.number = number;
+        this.baseRate = baseRate;
+    }
+}

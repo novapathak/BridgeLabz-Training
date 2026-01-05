a7c3ddd78ebed955cb0c5cfa52f111ac725659ba
novapathak <novapathak@gmail.com>
2026-01-05 11:16:12 +0530

    [Nova] : added movie file in linked list

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/linkedlist/movie/MovieNode.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.linkedlist.movie;
+
+class MovieNode {
+    String title, director;
+    int year;
+    double rating;
+    MovieNode prev, next;
+
+    MovieNode(String title, String director, int year, double rating) {
+        this.title = title;
+        this.director = director;
+        this.year = year;
+        this.rating = rating;
+    }
+}

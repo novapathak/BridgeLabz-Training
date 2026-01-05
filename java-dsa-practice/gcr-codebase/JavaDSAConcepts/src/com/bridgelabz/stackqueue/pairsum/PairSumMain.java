23662d5887c9a7b3365d9f2d032822d6efa7ce89
novapathak <novapathak@gmail.com>
2026-01-05 12:09:29 +0530

    [Nova] : added pair sum file in stack and queue

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/stackqueue/pairsum/PairSumMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.stackqueue.pairsum;
+
+public class PairSumMain {
+    public static void main(String[] args) {
+        int[] arr = {8, 7, 2, 5, 3, 1};
+        int target = 10;
+
+        System.out.println(PairSum.hasPair(arr, target));
+    }
+}

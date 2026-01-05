8b7e3bd371e18b6e3b8468a5ef51f4e0b071a94c
novapathak <novapathak@gmail.com>
2026-01-05 12:58:53 +0530

    [Nova] : added stock span file in stack and queue

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/stackqueue/stockspan/StockSpanMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.stackqueue.stockspan;
+
+public class StockSpanMain {
+    public static void main(String[] args) {
+        int[] prices = {100, 80, 60, 70, 60, 75, 85};
+        StockSpan.calculateSpan(prices);
+    }
+}

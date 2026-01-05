63e28c9f0eb4ffbc7c87d65b7812165b3409f00a
novapathak <novapathak@gmail.com>
2026-01-05 12:12:46 +0530

    [Nova] : added queue using stack  file in stack and queue

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/stackqueue/queueusingstack/QueueUsingStacksMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.stackqueue.queueusingstack;
+
+public class QueueUsingStacksMain {
+    public static void main(String[] args) {
+        QueueUsingStacks queue = new QueueUsingStacks();
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+    }
+}

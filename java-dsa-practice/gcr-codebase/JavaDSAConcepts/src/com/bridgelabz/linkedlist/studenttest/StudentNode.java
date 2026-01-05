c179e299359724b9f3edc176e378fbbab950157b
novapathak <novapathak@gmail.com>
2026-01-05 11:27:54 +0530

    [Nova] : added student file in linkedlist

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/linkedlist/studenttest/StudentNode.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.linkedlist.studenttest;
+
+class StudentNode {
+    int rollNo;
+    String name;
+    int age;
+    char grade;
+    StudentNode next;
+
+    // Constructor
+    StudentNode(int rollNo, String name, int age, char grade) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.next = null;
+    }
+}

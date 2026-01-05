2a9a4626b0fb8b3c1ba9965a471df29d0cb79318
novapathak <novapathak@gmail.com>
2026-01-05 12:06:19 +0530

    [Nova] : added consecutive sequence file in stack and queue

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/stackqueue/consecutivesequence/LongestConsecutiveMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.stackqueue.consecutivesequence;
+
+public class LongestConsecutiveMain {
+    public static void main(String[] args) {
+        int[] arr = {100, 4, 200, 1, 3, 2};
+        System.out.println(LongestConsecutive.longestSequence(arr));
+    }
+}
+

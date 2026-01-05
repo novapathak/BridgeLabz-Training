03e908ed99dc93533417aba1ac309ead6647c34d
novapathak <novapathak@gmail.com>
2026-01-05 11:36:20 +0530

    [Nova] : added texteditor file in linkedlist

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/linkedlist/texteditor/TextState.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.linkedlist.texteditor;
+
+class TextState {
+    String text;
+    TextState prev, next;
+
+    TextState(String text) {
+        this.text = text;
+    }
+}
+

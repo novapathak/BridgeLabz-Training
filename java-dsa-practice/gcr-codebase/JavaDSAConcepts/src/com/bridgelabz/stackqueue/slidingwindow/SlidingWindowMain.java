29803d372c951c55cd8e26cce73448910862cc84
novapathak <novapathak@gmail.com>
2026-01-05 12:55:05 +0530

    [Nova] : added sliding window   file in stack and queue

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/stackqueue/slidingwindow/SlidingWindowMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.stackqueue.slidingwindow;
+
+public class SlidingWindowMain {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, -1, -3, 5, 3, 6, 7};
+        SlidingWindowMax.findMax(arr, 3);
+    }
+}

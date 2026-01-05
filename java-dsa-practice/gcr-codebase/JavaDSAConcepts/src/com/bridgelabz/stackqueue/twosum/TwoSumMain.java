8df45c5fc26c12333dfe175a427b7c4f6943ded7
novapathak <novapathak@gmail.com>
2026-01-05 13:09:32 +0530

    [Nova] : added two sum file in stack and queue

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/stackqueue/twosum/TwoSumMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.stackqueue.twosum;
+
+public class TwoSumMain {
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int target = 9;
+
+        TwoSum.findIndices(arr, target);
+    }
+}

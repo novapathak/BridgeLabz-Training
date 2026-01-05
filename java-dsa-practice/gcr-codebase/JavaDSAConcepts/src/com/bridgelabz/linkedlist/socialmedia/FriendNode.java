9ee212b6901831183181d13abd7e1e9e5133d79b
novapathak <novapathak@gmail.com>
2026-01-05 11:23:50 +0530

    [Nova] : added social media file in linkedlist

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/linkedlist/socialmedia/FriendNode.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linkedlist.socialmedia;
+
+class FriendNode {
+    int friendId;
+    FriendNode next;
+
+    FriendNode(int friendId) {
+        this.friendId = friendId;
+    }
+}

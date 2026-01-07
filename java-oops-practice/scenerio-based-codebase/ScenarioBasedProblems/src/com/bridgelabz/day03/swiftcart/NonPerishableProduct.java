3c219835a4c1acafd8788ba13774ccfb42815606
novapathak <novapathak@gmail.com>
2026-01-07 22:33:53 +0530

    [Nova]: added swift cart  file in scenario based practice

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day03/swiftcart/NonPerishableProduct.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.day03.swiftcart;
+
+public class NonPerishableProduct extends Product {
+
+    public NonPerishableProduct(String name, double price) {
+        super(name, price, "Non-Perishable");
+    }
+
+    @Override
+    public double applyDiscount(double total) {
+        return total * 0.05; // 5% discount
+    }
+}

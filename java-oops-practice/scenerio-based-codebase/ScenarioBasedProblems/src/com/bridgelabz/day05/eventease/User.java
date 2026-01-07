7b43f430d020cdd7f4b35ae4f4ad9387a046a40d
novapathak <novapathak@gmail.com>
2026-01-07 22:48:18 +0530

    [Nova]: added Event file in scenario based practice

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day05/eventease/User.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.day05.eventease;
+
+public class User {
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
+

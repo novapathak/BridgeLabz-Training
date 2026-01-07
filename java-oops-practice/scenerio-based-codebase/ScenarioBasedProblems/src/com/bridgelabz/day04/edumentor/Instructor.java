92741181d232e1cea53aecc99cc1e4429b6d127b
novapathak <novapathak@gmail.com>
2026-01-07 22:38:27 +0530

    [Nova]: added edu mentor  file in scenario based practice

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day04/edumentor/Instructor.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.day04.edumentor;
+
+public class Instructor extends User {
+
+    public Instructor(String name, String email, int userId) {
+        super(name, email, userId);
+    }
+}

e98461da23e7f4a8e3284d14fc41a03115968ee2
novapathak <novapathak@gmail.com>
2026-01-08 00:24:07 +0530

    [Nova] : added PeakElementBinarySearch file

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/binarysearch/PeakElementBinarySearch.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.binarysearch;
+
+class PeakElementBinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 20, 4, 1};
+
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] < arr[mid + 1])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+
+        System.out.println("Peak Element: " + arr[left]);
+    }
+}

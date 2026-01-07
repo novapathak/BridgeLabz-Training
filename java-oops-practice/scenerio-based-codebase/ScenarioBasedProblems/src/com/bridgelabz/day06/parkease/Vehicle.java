f712a8f10426f420902e6cc429a069f15c701194
novapathak <novapathak@gmail.com>
2026-01-07 23:04:30 +0530

    [Nova]: added Park Ease file in scenario based practice

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day06/parkease/Vehicle.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day06.parkease;
+
+public abstract class Vehicle implements IPayable {
+    protected String vehicleNumber;
+    protected double baseRate;
+
+    public Vehicle(String vehicleNumber, double baseRate) {
+        this.vehicleNumber = vehicleNumber;
+        this.baseRate = baseRate;
+    }
+}

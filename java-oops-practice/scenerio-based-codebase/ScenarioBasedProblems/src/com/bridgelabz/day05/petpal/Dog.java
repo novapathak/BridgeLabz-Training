ac6272a1f4c26994ac53191d1c931b992f228e69
novapathak <novapathak@gmail.com>
2026-01-07 22:50:45 +0530

    [Nova]: added Petpal file in scenario based practice

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day05/petpal/Dog.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.day05.petpal;
+
+public class Dog extends Pet {
+
+    public Dog(String name, int age) {
+        super(name, "Dog", age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Woof Woof!");
+    }
+}

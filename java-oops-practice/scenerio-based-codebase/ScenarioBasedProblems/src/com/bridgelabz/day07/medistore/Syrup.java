58798e2b80cbe7e9284267425e93ed0a35d8debe
novapathak <novapathak@gmail.com>
2026-01-10 14:44:54 +0530

    [Nova] : added scnerio based question of day 07

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day07/medistore/Syrup.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.day07.medistore;
+
+import java.time.LocalDate;
+
+public class Syrup extends Medicine {
+
+    public Syrup(String name, double price, LocalDate expiryDate) {
+        super(name, price, expiryDate);
+    }
+
+    @Override
+    public boolean checkExpiry() {
+        return LocalDate.now().isAfter(expiryDate.minusDays(10));
+    }
+}

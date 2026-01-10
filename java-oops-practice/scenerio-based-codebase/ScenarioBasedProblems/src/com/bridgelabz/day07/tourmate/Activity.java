cc02bf39441fceee4d7884689496b368115eb2ed
novapathak <novapathak@gmail.com>
2026-01-10 14:47:02 +0530

    [Nova] : added scnerio based question of day 07

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day07/tourmate/Activity.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.day07.tourmate;
+
+public class Activity{
+	
+	private double activityCost;
+	public Activity(double activityCost){
+		this.activityCost = activityCost;
+	}
+	public double getActivityCost() {
+		return activityCost;
+	}
+
+}

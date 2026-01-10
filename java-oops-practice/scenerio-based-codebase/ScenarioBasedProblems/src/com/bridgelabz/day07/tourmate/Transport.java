6f637c34a610fd39050c5b28680bb43b60733010
novapathak <novapathak@gmail.com>
2026-01-10 14:48:17 +0530

    [Nova] : added scnerio based question of day 07

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day07/tourmate/Transport.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.day07.tourmate;
+
+public class Transport  {
+
+	private double transportCost;
+	public Transport(double transportCost){
+		this.transportCost = transportCost;
+	}
+	public double getTransportCost() {
+		return transportCost;
+	}
+
+}

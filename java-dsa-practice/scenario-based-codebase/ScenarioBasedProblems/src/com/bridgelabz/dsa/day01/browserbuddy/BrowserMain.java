ca2e1c38893017e36661ad2615e9ce7c16d47a35
novapathak <novapathak@gmail.com>
2026-01-12 11:44:10 +0530

    [Nova]: added file in dsa scenario based

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day01/browserbuddy/BrowserMain.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.dsa.day01.browserbuddy;
+
+public class BrowserMain {
+    public static void main(String[] args) {
+
+        BrowserHistory browser = new BrowserHistory();
+
+        browser.visit("google.com");
+        browser.visit("github.com");
+        browser.visit("stackoverflow.com");
+
+        browser.back();
+        browser.forward();
+
+        browser.closeTab();
+        browser.restoreTab();
+    }
+}

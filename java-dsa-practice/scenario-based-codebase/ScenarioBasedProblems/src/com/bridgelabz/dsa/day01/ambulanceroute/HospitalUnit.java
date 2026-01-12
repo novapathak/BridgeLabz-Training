6def20ef593c9eaaa37baf1d097e1377552bc09d
novapathak <novapathak@gmail.com>
2026-01-12 11:35:23 +0530

    [Nova]: added file in dsa scenario based

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day01/ambulanceroute/HospitalUnit.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.dsa.day01.ambulanceroute;
+
+public class HospitalUnit {
+    String name;
+    boolean available;
+    HospitalUnit next;
+
+    HospitalUnit(String name, boolean available) {
+        this.name = name;
+        this.available = available;
+    }
+}

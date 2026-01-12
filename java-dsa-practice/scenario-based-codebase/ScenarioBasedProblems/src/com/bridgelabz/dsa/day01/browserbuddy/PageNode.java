9d54c7690fb5fbf2b19e6bd7a72f1477f07b581d
novapathak <novapathak@gmail.com>
2026-01-12 11:44:24 +0530

    [Nova]: added file in dsa scenario based

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day01/browserbuddy/PageNode.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.dsa.day01.browserbuddy;
+
+public class PageNode {
+    String url;
+    PageNode prev;
+    PageNode next;
+
+    PageNode(String url) {
+        this.url = url;
+    }
+}

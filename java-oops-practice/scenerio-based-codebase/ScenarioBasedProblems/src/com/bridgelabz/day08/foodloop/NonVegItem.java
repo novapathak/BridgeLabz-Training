5c19b1564ce38e4924bd6d3192384256e47c92bb
novapathak <novapathak@gmail.com>
2026-01-12 13:20:16 +0530

    [Nova]: added file in OOPs scenario based question

---
+++ java-oops-practice/scenerio-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day08/foodloop/NonVegItem.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.day08.foodloop;
+
+class NonVegItem extends FoodItem {
+    public NonVegItem(String name, double price, int stock) {
+        super(name, "Non-Veg", price, stock);
+    }
+}

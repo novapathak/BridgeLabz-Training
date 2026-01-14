456c31d2f5f862ac9f99fd8743c9e0280c9990c0
novapathak <novapathak@gmail.com>
2026-01-15 01:10:05 +0530

    [Nova]: added scenario based question in dsa

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day02/traincompanion/Compartment.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.dsa.day02.traincompanion;
+
+public class Compartment {
+
+	String name;
+	Compartment next;
+	Compartment prev;
+	
+	public Compartment(String name) {
+		this.name = name;
+		this.next = null;
+		this.prev = null;
+	}
+	
+	
+}

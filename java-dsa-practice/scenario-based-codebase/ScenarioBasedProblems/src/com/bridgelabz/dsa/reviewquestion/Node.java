6e6a2ef5cc245fcfdd3c0c4d3e1cc2c91066ad5b
novapathak <novapathak@gmail.com>
2026-01-17 02:46:11 +0530

    [Nova]: added review question file in dsa scenario based question

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/reviewquestion/Node.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.dsa.reviewquestion;
+import java.util.*;
+public class Node {
+	int data;
+	Node next;
+	Node(int data){
+		this.data = data;
+		this.next = null;
+	}
+	
+	
+
+}

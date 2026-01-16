a52c6ec781dd57822c8b3d1a8b64ef31ef019084
novapathak <novapathak@gmail.com>
2026-01-17 02:45:31 +0530

    [Nova]: added review question file in dsa scenario based question

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/reviewquestion/LinkedList.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.dsa.reviewquestion;
+import java.util.*;
+public class LinkedList {
+	public static void main(String[]args) {
+		Implement ll = new Implement();
+		ll.display();
+	    ll.insertElement(10, 0);
+	    ll.insertElement(11, 1);
+	    ll.insertElement(12, 2);
+	    ll.insertElement(13, 3);
+	    
+	    ll.display();
+	   ll.deleteElement(2);
+	    ll.display();
+
+	}
+
+}

bbf919bf0fd133a6de8bc6c90df8214d0171afe1
novapathak <novapathak@gmail.com>
2026-01-17 02:40:52 +0530

    [Nova]: added file in dsa scenario based question

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day02/callcenter/CallCenterMain.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.dsa.day02.callcenter;
+
+public class CallCenterMain {
+	
+
+	    public static void main(String[] args) {
+
+	        CallCenter c = new CallCenter();
+
+	        c.call("Nova", false);
+	        c.call("Ashi", true);
+	        c.call("Radhika", false);
+	        c.call("Nalinee", true);
+
+	        c.attend();
+	        c.attend();
+	        c.attend();
+
+	        c.showCallCount();
+	    }
+	}
+

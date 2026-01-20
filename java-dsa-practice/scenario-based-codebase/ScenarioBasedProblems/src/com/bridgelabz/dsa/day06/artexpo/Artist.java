f5d1bedb571f387235928e80e294dece7ffdebcd
novapathak <novapathak@gmail.com>
2026-01-20 14:52:36 +0530

    [Nova]: added file in dsa scenario based question

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day06/artexpo/Artist.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.dsa.day06.artexpo;
+
+class Artist {
+    int registrationTime;
+    Artist(int registrationTime) {
+        this.registrationTime = registrationTime;
+    }
+}

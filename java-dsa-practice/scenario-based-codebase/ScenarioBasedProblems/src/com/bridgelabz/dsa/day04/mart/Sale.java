1889b9db24dc0d653d5c88178df563ab79f77bd0
novapathak <novapathak@gmail.com>
2026-01-20 14:46:42 +0530

    [Nova]: added file in dsa scenario based question

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day04/mart/Sale.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.dsa.day04.mart;
+
+class Sale {
+    String date;
+    int amount;
+
+    Sale(String date, int amount) {
+        this.date = date;
+        this.amount = amount;
+    }
+}
+

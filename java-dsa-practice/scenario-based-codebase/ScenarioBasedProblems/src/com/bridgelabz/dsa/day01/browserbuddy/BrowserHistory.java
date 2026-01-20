29092b6d73fa0bbbd9c7c7845f7becfb5ee36173
novapathak <novapathak@gmail.com>
2026-01-20 15:04:41 +0530

    [Nova]: added file in dsa scenario  based question

--- java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day01/browserbuddy/BrowserHistory.java
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day01/browserbuddy/BrowserHistory.java
@@ -3,31 +3,27 @@ package com.bridgelabz.dsa.day01.browserbuddy;
 import java.util.Stack;
 
 public class BrowserHistory {
-
     PageNode current;
     Stack<String> closedTabs = new Stack<>();
-
     void visit(String url) {
         PageNode page = new PageNode(url);
-
         if (current != null) {
-            current.next = page;
-            page.prev = current;
+            current.next= page;
+            page.prev= current;
         }
         current = page;
     }
-
     void back() {
-        if (current != null && current.prev != null) {
+        if ( current != null && current.prev!= null) {
             current = current.prev;
             System.out.println(current.url);
         }
     }
 
     void forward() {
-        if (current != null && current.next != null) {
+        if (  current != null && current.next != null) {
             current = current.next;
-            System.out.println(current.url);
+            System.out.println( current.url);
         }
     }
 

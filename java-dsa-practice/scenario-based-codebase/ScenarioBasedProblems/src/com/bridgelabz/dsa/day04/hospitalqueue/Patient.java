03d60d651c9aa1f34b540e4ba34f7d4e762f4e09
novapathak <novapathak@gmail.com>
2026-01-20 14:45:15 +0530

    [Nova]: added file in dsa scenario based question

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day04/hospitalqueue/Patient.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.dsa.day04.hospitalqueue;
+
+public class Patient {
+	String name;
+	int criticality;
+	Patient(String name, int criticality){
+		this.name = name;
+		this.criticality = criticality;
+
+	}
+
+}

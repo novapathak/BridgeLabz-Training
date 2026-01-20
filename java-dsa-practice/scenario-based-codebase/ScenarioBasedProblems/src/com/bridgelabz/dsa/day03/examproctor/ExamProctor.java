8f00905f146ec3debda031a72b56f12309edb30e
novapathak <novapathak@gmail.com>
2026-01-20 11:30:48 +0530

    [Nova]: added file in dsa scenario based question

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day03/examproctor/ExamProctor.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.dsa.day03.examproctor;
+
+import java.util.*;
+
+class ExamProctor {
+
+    Stack<Integer> navigation = new Stack<>();
+    HashMap<Integer, String> answers = new HashMap<>();
+    HashMap<Integer, String> correctAnswers = new HashMap<>();
+
+    // Constructor to store correct answer
+    ExamProctor() {
+      correctAnswers.put(1, "A");
+        correctAnswers.put(2, "B");
+        correctAnswers.put(3, "C");
+    }
+
+    // question visit
+    void visitQuestion(int qId) {
+        navigation.push(qId);
+        System.out.println("Visited Question " + qId);
+    }
+    // Answer a question
+    void answerQuestion(int qId, String answer) {
+        answers.put(qId, answer);
+        System.out.println("Answered Q" + qId + " = " + answer);
+    }
+
+    // evaluate score
+    int calculateScore() {
+        int score = 0;
+        for (int qId : correctAnswers.keySet()) {
+            if (correctAnswers.get(qId).equals(answers.get(qId))) {
+                score++;
+            }
+        }
+        return score;
+    }
+}

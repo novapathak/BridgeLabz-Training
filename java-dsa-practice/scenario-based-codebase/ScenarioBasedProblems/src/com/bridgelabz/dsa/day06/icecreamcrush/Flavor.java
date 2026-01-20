e5f3de12ecd49abf9b693ef39989b82d3ec03748
novapathak <novapathak@gmail.com>
2026-01-20 14:57:12 +0530

    [Nova]: added file in dsa scenario based question

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day06/icecreamcrush/Flavor.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.dsa.day06.icecreamcrush;
+
+class Flavor {
+    String name;
+    int sold;
+    Flavor(String name, int sold) {
+        this.name = name;
+        this.sold = sold;
+    }
+}

e19bf81b67b4e00c010af871b1f2faac3f0113f2
novapathak <novapathak@gmail.com>
2026-01-20 15:06:21 +0530

    [Nova]: modified file in dsa scenario  based question

--- java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day05/cinemaHouse/CinemaHouse.java
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day05/cinemaHouse/CinemaHouse.java
@@ -3,23 +3,20 @@ package com.bridgelabz.dsa.day05.cinemaHouse;
 class CinemaHouse {
 
     public static void main(String[] args) {
-
-        int[] arr = {1800, 1400, 2100, 1200, 1600};
+        int[]arr={1800, 1400, 2100, 1200, 1600};
 
         // Bubble Sort
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
+        for (int i=0;i<arr.length-1;i++) {
+            for (int j=0;j<arr.length-1-i;j++) {
 
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                   arr[j] = arr[j + 1];
-                   arr[j + 1] = temp;
+                if (arr[j]>arr[j+1]) {
+                    int temp=arr[j];
+                   arr[j]=arr[j+ 1];
+                   arr[j+1]= temp;
                 }
             }
         }
-
-       
-        System.out.println("Sorted Movie Show Times:");
+       System.out.println("Sorted Movie Show Times:");
         for (int time : arr) {
             System.out.println(time);
         }

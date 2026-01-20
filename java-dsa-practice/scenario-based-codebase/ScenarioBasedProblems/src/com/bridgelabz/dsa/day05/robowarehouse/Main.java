7e427c4c9825e971b06e4ba71f4d43e6f68a96c4
novapathak <novapathak@gmail.com>
2026-01-20 14:49:53 +0530

    [Nova]: added file in dsa scenario based question

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day05/robowarehouse/Main.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.dsa.day05.robowarehouse;
+
+class Main {
+
+    void insertionSort(PackageItem[] items) {
+        for (int i=1;i<items.length;i++) {
+            PackageItem key= items[i];
+            int j =i-1;
+
+            while (j >=0&&items[j].weight>key.weight) {
+                items[j+1]=items[j];
+                j--;
+            }
+
+            items[j+1]= key;
+       }
+    }
+    void display(PackageItem[] items) {
+        for (PackageItem p:items) {
+            System.out.print(p.weight + " ");
+        }
+        System.out.println();
+    }
+}

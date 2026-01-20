b503ccd7079a355b348ffe8be568701344fb8317
novapathak <novapathak@gmail.com>
2026-01-20 14:44:25 +0530

    [Nova]: added file in dsa scenario based question

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day04/examcell/Student.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.dsa.day04.examcell;
+
+class Student {
+    String name;
+    int score;
+
+    Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}

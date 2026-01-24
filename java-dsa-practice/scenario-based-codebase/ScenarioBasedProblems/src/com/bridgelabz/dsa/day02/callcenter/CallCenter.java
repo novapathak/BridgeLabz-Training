d7532ef942c66a27b4cca6a2954335604d4be228
novapathak <novapathak@gmail.com>
2026-01-24 16:33:09 +0530

    [Nova]: added file in dsa scenario based questions

--- java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day02/callcenter/CallCenter.java
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day02/callcenter/CallCenter.java
@@ -1,31 +1,31 @@
 package com.bridgelabz.dsa.day02.callcenter;
 import java.util.*;
 public class CallCenter {
-	Queue<String> normalQ = new LinkedList<>();
-	Queue<String> vipQ = new LinkedList<>();
+	Queue<String>normalQ=new LinkedList<>();
+	Queue<String>vipQ=new LinkedList<>();
 	HashMap<String, Integer> callCount = new HashMap<>();
 
 	//Adding  call
-	void call(String name , boolean isVIP) {
-		callCount.put(name , callCount.getOrDefault(name, 0)+1);
+	void call(String name,boolean isVIP) {
+		callCount.put(name ,callCount.getOrDefault(name, 0)+1);
     if(isVIP) {
 			vipQ.add(name);
-			System.out.println(name + "VIP added");
+			System.out.println(name+"VIP added");
 		}
 		else {
 			normalQ.add(name);
-			System.out.println(name + "Normal added");
+			System.out.println(name+"Normal added");
 		}
 	
 	}
 	void attend() {
 		
 	 if (!vipQ.isEmpty()) {
-	            System.out.println("Attending VIP: " + vipQ.poll());
+	            System.out.println("Attending VIP: "+vipQ.poll());
 
 	        } 
 		 else if (!normalQ.isEmpty()) {
-	            System.out.println("Attending: " + normalQ.poll());
+	            System.out.println("Attending: "+normalQ.poll());
 	            } 
 		 else {
 	            System.out.println("No calls waiting");

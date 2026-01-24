7608b225c939deaf680651f002c4dac746c63cf7
novapathak <novapathak@gmail.com>
2026-01-24 16:34:28 +0530

    [Nova]: added file in dsa scenario based questions

--- java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day03/examproctor/Main.java
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day03/examproctor/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         ExamProctor exam = new ExamProctor();
-
+       
         exam.visitQuestion(1);
         exam.answerQuestion(1, "A");
         exam.visitQuestion(2);
@@ -12,6 +12,7 @@ public class Main {
         exam.visitQuestion(3);
         exam.answerQuestion(3, "C");
 
+     
         int score = exam.calculateScore();
         System.out.println("Total score = " + score);
     }

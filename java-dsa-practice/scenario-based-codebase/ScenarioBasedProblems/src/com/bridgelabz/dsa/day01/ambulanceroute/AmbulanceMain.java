59b6e31ebd0ed84662a3afb126ff34bcec77e679
novapathak <novapathak@gmail.com>
2026-01-24 16:30:56 +0530

    [Nova]: added file in dsa scenario based questions

--- java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day01/ambulanceroute/AmbulanceMain.java
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/dsa/day01/ambulanceroute/AmbulanceMain.java
@@ -1,14 +1,14 @@
 package com.bridgelabz.dsa.day01.ambulanceroute;
-
+import java.util.*;
 public class AmbulanceMain {
     public static void main(String[] args) {
 
         AmbulanceRoute route = new AmbulanceRoute();
 
-        route.addUnit("Emergency", false);
-        route.addUnit("Radiology", false);
+       route.addUnit("Emergency", false);
+       /* route.addUnit("Radiology", false);
         route.addUnit("Surgery", true);
-        route.addUnit("ICU", false);
+        route.addUnit("ICU", false);*/
         route.findAvailableUnit();
         route.removeUnit("Radiology");
         route.findAvailableUnit();

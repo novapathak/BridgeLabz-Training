101c249dc8bb46ba12a4cf33ce8be2157d5e6739
novapathak <novapathak@gmail.com>
2026-02-02 01:57:13 +0530

    [Nova]: added in bankingsystem

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcepts/src/com/bridgelabz/collections/bankingsystem/WithdrawReq.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.collections.bankingsystem;
+
+public class WithdrawReq {
+
+		int acc;
+		int amt;
+
+  public WithdrawReq (int acc,int amt){
+	  	this.acc=acc;
+	  	this.amt=amt;
+}
+
+}

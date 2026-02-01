57aa168746a965b43ea87498b3dc88eaa0e02c9b
novapathak <novapathak@gmail.com>
2026-02-02 01:57:27 +0530

    [Nova]: added in exceptions

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcepts/src/com/bridgelabz/exceptions/AgeValidation.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.exceptions;
+import java.util.*;
+class InvalidAgeException extends Exception {
+    InvalidAgeException(String msg) {
+        super(msg);
+    }
+}
+
+public class AgeValidation {
+
+    static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18)
+            throw new InvalidAgeException("Age must be 18 or above");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int age = sc.nextInt();
+            validateAge(age);
+            System.out.println("Access granted!");
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+

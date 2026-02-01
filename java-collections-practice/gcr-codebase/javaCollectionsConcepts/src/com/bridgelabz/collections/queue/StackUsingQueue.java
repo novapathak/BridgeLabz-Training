f16150c34594df01229e8295e279a48c7aa36fa3
novapathak <novapathak@gmail.com>
2026-02-02 01:57:25 +0530

    [Nova]: added in queue

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcepts/src/com/bridgelabz/collections/queue/StackUsingQueue.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.collections.queue;
+import java.util.*;
+
+class StackUsingQueues {
+    Queue<Integer> q1 = new LinkedList<>();
+    Queue<Integer> q2 = new LinkedList<>();
+
+    public void push(int x) {
+        q2.add(x); 
+        while (!q1.isEmpty()) {
+            q2.add(q1.remove());
+        }
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+    public int pop() {
+        return q1.remove();
+    }
+    public int top() {
+        return q1.peek();
+    }
+}
+
+public class StackUsingQueue {
+    public static void main(String[] args) {
+        StackUsingQueues s = new StackUsingQueues();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        System.out.println(s.pop());
+    }
+}
+
+
+

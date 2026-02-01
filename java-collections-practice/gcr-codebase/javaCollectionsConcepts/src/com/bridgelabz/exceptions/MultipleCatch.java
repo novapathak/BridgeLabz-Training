d787cc0a7aab42bd05522bd28060ce8dbcc13392
novapathak <novapathak@gmail.com>
2026-02-02 01:57:29 +0530

    [Nova]: added in exceptions

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcepts/src/com/bridgelabz/exceptions/MultipleCatch.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.exceptions;
+
+public class MultipleCatch {
+    public static void main(String[] args) {
+        try {
+            int[] arr = {10, 20, 30};
+            int index = 5;
+            System.out.println(arr[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index!");
+        } catch (NullPointerException e) {
+            System.out.println("Array is not initialized!");
+        }
+    }
+}
+
+
+
+

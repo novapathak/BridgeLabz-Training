df6cf0626bc6352cb333ee7fa57115ad824ce357
novapathak <novapathak@gmail.com>
2026-02-02 01:57:30 +0530

    [Nova]: added in mealplan

---
+++ java-collections-practice/gcr-codebase/javaCollectionsConcepts/src/com/bridgelabz/generics/mealplan/HighProtein.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.generics.mealplan;
+
+public class HighProtein implements MealPlan {
+
+	public void prepareMeal() {
+		System.out.println(" Preparing high protein meal ");
+	}
+}

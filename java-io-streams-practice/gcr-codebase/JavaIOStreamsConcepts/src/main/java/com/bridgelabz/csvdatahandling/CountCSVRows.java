42a5f6d89ae98f5dc19c82d04e7694cdcfc992f3
novapathak <novapathak@gmail.com>
2026-02-02 15:20:36 +0530

    [Nova] : added CountCSVRows.java in csvdatahandling

---
+++ java-io-streams-practice/gcr-codebase/JavaIOStreamsConcepts/src/main/java/com/bridgelabz/csvdatahandling/CountCSVRows.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.csvdatahandling;
+
+import java.io.*;
+
+class CountCSVRows {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("students.csv"));
+        int count = 0;
+
+        br.readLine(); 
+        while (br.readLine() != null) {
+            count++;
+        }
+
+        br.close();
+        System.out.println("Total Records: " + count);
+    }
+}

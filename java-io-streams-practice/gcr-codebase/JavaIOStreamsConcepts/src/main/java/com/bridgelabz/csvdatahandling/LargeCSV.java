22ee092db6a95b3d59fcd8a90bafd4c4a800ad6b
novapathak <novapathak@gmail.com>
2026-02-02 15:20:39 +0530

    [Nova] : added LargeCSV.java in csvdatahandling

---
+++ java-io-streams-practice/gcr-codebase/JavaIOStreamsConcepts/src/main/java/com/bridgelabz/csvdatahandling/LargeCSV.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.csvdatahandling;
+
+import java.io.*;
+
+class LargeCSV {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("large.csv"));
+        int count = 0;
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            count++;
+            if (count % 100 == 0) {
+                System.out.println("Processed: " + count);
+            }
+        }
+        br.close();
+    }
+}

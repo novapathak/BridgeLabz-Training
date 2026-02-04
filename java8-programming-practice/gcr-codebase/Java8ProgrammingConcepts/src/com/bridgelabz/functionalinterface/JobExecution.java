388261da4fe201b35874ec97d4a01e1354765d56
novapathak <novapathak@gmail.com>
2026-02-05 00:33:57 +0530

    [Nova] : added JobExecution.java in functionalinterface

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/bridgelabz/functionalinterface/JobExecution.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.functionalinterface;
+
+public class JobExecution {
+	public static void main(String[] args) {
+		
+		Runnable job = () -> {System.out.println("Background job started...");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+            	 System.out.println("Error");
+            }
+            System.out.println("Background job completed.");
+        };
+        Thread thread = new Thread(job);
+        thread.start();
+
+        System.out.println("Main thread continues...");
+    }
+		
+	
+}

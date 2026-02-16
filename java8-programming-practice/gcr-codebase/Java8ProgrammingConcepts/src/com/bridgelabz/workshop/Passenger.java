77fc39f526f0cea11b78023bc0ebdb9b3adab6ee
novapathak <novapathak@gmail.com>
2026-02-16 13:20:54 +0530

    [Nova] : added Passenger.java in workshop

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/bridgelabz/workshop/Passenger.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.workshop;
+
+public class Passenger {
+
+    private String name;
+    private String route;
+    private double fare;
+    private boolean peakTime;
+
+    public Passenger(String name, String route, double fare, boolean peakTime) {
+        this.name = name;
+        this.route = route;
+        this.fare = fare;
+        this.peakTime = peakTime;
+    }
+
+    public String getRoute() { return route; }
+    public double getFare() { return fare; }
+    public boolean isPeakTime() { return peakTime; }
+}
